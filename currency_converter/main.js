da55e7636573ed1b564ee7ad744d3fed4d88ffe5
moiz <moizaman905@gmail.com>
2024-04-09 18:09:55 -0700

    Currency Converter

---
+++ currency_converter/main.js
@@ -0,0 +1,38 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+console.log(chalk.yellow("\n \tWelcome To Moiz Ahmed Sahikh - Currency Convertor App\n"));
+const currency = {
+    USD: 1, //Base value
+    PAK: 277,
+    IND: 83.23,
+    UAE: 3.5,
+    EUR: 0.9,
+    CHN: 7.23,
+    TUR: 32.19
+};
+let answer = await inquirer.prompt([
+    {
+        name: "from",
+        message: "Enter from Currency",
+        type: "list",
+        choices: ["USD", "PAK", "IND", "UAE", "EUR", "CHN", "TUR"],
+    },
+    {
+        name: "to",
+        message: "Enter to Currency",
+        type: "list",
+        choices: ["USD", "PAK", "IND", "UAE", "EUR", "CHN", "TUR"],
+    },
+    {
+        name: "amount",
+        message: "Enter your Amount",
+        type: "number",
+    },
+]);
+let fromAnswer = currency[answer.from];
+let toAnswer = currency[answer.to];
+let amountAnwser = answer.amount;
+let baseAmount = amountAnwser / fromAnswer;
+let convertAmount = baseAmount * toAnswer;
+console.log(chalk.gray(`Converted Amount = ${chalk.red(convertAmount.toFixed(2))}`));
